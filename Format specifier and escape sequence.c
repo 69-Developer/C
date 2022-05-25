42dd678b3e83767f779f262d8ad634584281e272
Ezra <sumit.developer.69@gmail.com>
2022-05-25 14:55:38 +0530

    Escape sequence and comments

---
+++ Format specifier and escape sequence.c
@@ -0,0 +1,43 @@
+#include "stdio.h"
+#define areaofcircle = "4";
+
+int main(){
+
+    // write code here
+    float a;
+    a = 55.5;
+    printf("%f",a);
+    int b;
+    b = 4;
+    printf("\n%d",b);
+
+    // Printing floating pointer numbers
+    
+    float xyz = 36.452;
+
+    printf("%0.2f",xyz);
+
+    // format specifiers
+
+    /*
+    %d - Integers
+    %f - Floating point number
+    %c - Character / string
+    %l - long
+    %lf - double
+    %Lf - long double
+    */
+
+//    Setting up constants
+
+    // 1. 
+    const float PI = 3.14;
+
+    printf("%0.2f",PI);
+
+    // Another one
+    // areaofcircle = 69;
+
+    printf("\a",PI);
+    // by #define
+}
